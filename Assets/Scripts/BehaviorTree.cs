f7cf1fb8cad559605118ca55438b17b9548d74eb
Daniel Revis <drevis@gmu.edu>
2020-12-10 19:02:38 -0500

    Implemented simple Behavior Tree

---
+++ Assets/Scripts/BehaviorTree.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace BTree
+{
+    public class Node
+    {
+        public List<Node> children;
+
+        public void addChild(Node node)
+        {
+            children.Add(node);
+        }
+
+        public bool run() { return false; }
+    }
+
+    class SelectorNode : Node
+    {
+        new bool run()
+        {
+            foreach (Node child in children)
+            {
+                if (child.run())
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    };
+
+    class SequenceNode : Node
+    {
+        new bool run()
+        {
+            foreach (Node child in children)
+            {
+                if (!child.run())
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    };
+
+    public class BehaviorTree
+    {
+        Node root;
+
+        public BehaviorTree(Node node) { root = node; }
+
+        public bool run()
+        {
+            if (this.root == null)
+                return false;
+
+            return root.run();
+        }
+    }
+}
---
+++ Assets/Scripts/Scientist.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BTree;
+
+public class Scientist : MonoBehaviour
+{
+    BehaviorTree behavior;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Node root = null;
+
+        this.behavior = new BehaviorTree(root);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        behavior.run();
+    }
+}
